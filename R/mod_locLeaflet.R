37b0dfee306a8e2342579a353e27ed5c454d0e90
ztgroton <ztgroton@gmail.com>
2024-04-23 18:44:23 -0400

    feat: enabled reactive drawing of map markers
    Built new reactive logic for adding and removing markers for store and competitor locations. Provides visual representation of locations selected using 'storeLoc' and 'compLoc' modules.

--- R/mod_locLeaflet.R
+++ R/mod_locLeaflet.R
@@ -31,6 +31,9 @@ locLeafletUI <- function(id) {
 
 #' Server Function for Location Leaflet Module
 #'
+#' @import shiny
+#' @importFrom rlang .data
+#'
 #' @param id module id
 #'
 #' @return shiny reactive
@@ -49,6 +52,18 @@ locLeafletServer <- function(id) {
     # * comp_loc ----
     comp_loc <- compLocServer('comp_loc')
 
+    # ________ ----
+    # reactive values ----
+
+    # * store_ids ----
+    store_ids <- reactiveValues(curr = vector('character'), add = vector('character'), drop = vector('character'))
+
+    # * comp_ids ----
+    comp_ids <- reactiveValues(curr = vector('character'), add = vector('character'), drop = vector('character'))
+
+    # * assoc_ids ----
+    assoc_ids <- reactiveValues(curr = vector('character'), add = vector('character'), drop = vector('character'))
+
     # _______ ----
     # outputs ----
 
@@ -66,6 +81,118 @@ locLeafletServer <- function(id) {
         ) %>%
         leaflet::setView(-98.5795, 37.8283, zoom = 5)
 
+
+    })
+
+    map_proxy <- leaflet::leafletProxy(mapId = "map", session = session)
+
+    # _________ ----
+    # observers ----
+
+    # * store_loc() ----
+    observeEvent(store_loc(), {
+
+      selected_ids <- store_loc() %>%
+        dplyr::mutate(layer_id = paste0('store-', .data$store_key)) %>%
+        dplyr::pull(.data$layer_id) %>% unique()
+
+      # update 'store_ids'
+      store_ids$add <- setdiff(selected_ids, store_ids$curr)
+      store_ids$drop <- setdiff(store_ids$curr, selected_ids)
+      store_ids$curr <- selected_ids
+
+    })
+
+    # ** store_ids$add ----
+    observeEvent(store_ids$add, {
+
+      if (isTRUE(length(store_ids$add) > 0)) {
+
+        add_data <- store_loc() %>%
+          dplyr::mutate(layer_id = paste0('store-', .data$store_key)) %>%
+          dplyr::filter(.data$layer_id %in% store_ids$add)
+
+        map_proxy %>%
+          leaflet::addAwesomeMarkers(
+            data = add_data,
+            group = "store",
+            layerId = ~layer_id,
+            lng = ~longitude,
+            lat = ~latitude,
+            icon = leaflet::makeAwesomeIcon(icon = 'star', markerColor = 'purple', iconColor = 'black')
+          )
+
+        store_ids$add <- vector('character')
+
+      }
+
+    })
+
+    # ** store_ids$drop ----
+    observeEvent(store_ids$drop, {
+
+      if (isTRUE(length(store_ids$drop) > 0)) {
+
+        map_proxy %>% leaflet::removeMarker(layerId = store_ids$drop)
+        store_ids$drop <- vector('character')
+
+      }
+
+    })
+
+    # * comp_loc() ----
+    observeEvent(comp_loc(), {
+
+      selected_ids <- comp_loc() %>%
+        dplyr::mutate(layer_id = paste0('comp-', .data$comp_key)) %>%
+        dplyr::pull(.data$layer_id) %>% unique()
+
+      # update 'comp_ids'
+      comp_ids$add <- setdiff(selected_ids, comp_ids$curr)
+      comp_ids$drop <- setdiff(comp_ids$curr, selected_ids)
+      comp_ids$curr <- selected_ids
+
+    })
+
+    # ** comp_ids$add ----
+    observeEvent(comp_ids$add, {
+
+      if (isTRUE(length(comp_ids$add) > 0)) {
+
+        add_data <- comp_loc() %>%
+          dplyr::left_join(storeMap::banner_colors, by = 'banner') %>%
+          dplyr::mutate(layer_id = paste0('comp-', .data$comp_key)) %>%
+          dplyr::filter(.data$layer_id %in% comp_ids$add)
+
+        map_proxy %>%
+          leaflet::addCircleMarkers(
+            data = add_data,
+            group = "comp",
+            layerId = ~layer_id,
+            lng = ~longitude,
+            lat = ~latitude,
+            color = ~color,
+            label = ~banner,
+            stroke = TRUE,
+            fillOpacity = 1,
+            radius = 4
+          )
+
+        comp_ids$add <- vector('character')
+
+      }
+
+    })
+
+    # ** comp_ids$drop ----
+    observeEvent(comp_ids$drop, {
+
+      if (isTRUE(length(comp_ids$drop) > 0)) {
+
+        map_proxy %>% leaflet::removeMarker(layerId = comp_ids$drop)
+        comp_ids$drop <- vector('character')
+
+      }
 
     })
 
